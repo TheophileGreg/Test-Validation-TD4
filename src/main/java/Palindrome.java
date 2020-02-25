30aa21c0b1c89f8b0ed834fc5819631dacbfabac
TheophileGreg <theophile.gregoire@etu.umontpellier.fr>
2020-02-25 12:14:20 +0100

    palindrome  class fini

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -13,6 +13,15 @@ public class Palindrome {
     }
 
     public static boolean isPalindrome(String m){
+        int i1 = 0;
+        int i2 = m.length() + 1;
+        while (i1 < i2){
+            if(m.charAt(i1) != m.charAt(i2)){
+                return false;
+            }
+            i1++;
+            i2--;
+        }
         return true;
     }
 }
