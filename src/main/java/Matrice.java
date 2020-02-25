7a7b19a80c5c9cdd41de8addb02c38783a56593f
TheophileGreg <theophile.gregoire@etu.umontpellier.fr>
2020-02-25 11:24:44 +0100

    Test initMatrice Done

--- src/main/java/Matrice.java
+++ src/main/java/Matrice.java
@@ -1,2 +1,6 @@
 public class Matrice {
+
+    public static int[][] initMatrice(int M, int N, int A, int B){
+        return new int[1][1];
+    }
 }
--- src/test/java/MatriceTest.java
+++ src/test/java/MatriceTest.java
@@ -1,5 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatriceTest {
+    @BeforeEach
+    void setUp() {
+        int[][] Y = initMatrice(M,N,A,B);
+    }
 
+    void matrice_test(M, N, A, B) {
+        assertEquals( M, Y.length);
+        for(int i = 0; i < M; i++){
+            assertEquals(N, Y[i]);
+            for (int j = 0; j < N; j++){
+                assertTrue( A <= Y[i][j] <= B);
+            }
+        }
+    }
 }
