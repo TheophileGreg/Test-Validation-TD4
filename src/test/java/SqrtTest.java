4e41b4d68a617cc220e4a8ab8ec8ab425730023b
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-02-25 11:25:31 +0100

    Add others test

--- src/test/java/SqrtTest.java
+++ src/test/java/SqrtTest.java
@@ -15,12 +15,17 @@ class SqrtTest {
     }
 
     @Test
-    void racineEntre11() {
-
-        float [] attendu = new float[] {1f} ;
-        assertEquals(attendu, Sqrt.racine(1,1f));
-
+    void racineEntre1et1() {
+        double [] attendu = new double[] {1.0} ;
+        assertEquals(attendu, Sqrt.racine(1,1));
     }
 
+    @Test
+    void racineEntre1et5() {
+
+        double [] attendu = new double[] {Math.sqrt(1), Math.sqrt(2), Math.sqrt(3), Math.sqrt(4), Math.sqrt(5)} ;
+        assertEquals(attendu, Sqrt.racine(1,5));
+
+    }
 
 }
