b59f7743c826652482d7b5e0dbd9efdeb140aed8
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2020-02-25 11:14:48 +0100

    Add first test

--- src/main/java/Sqrt.java
+++ src/main/java/Sqrt.java
@@ -1,2 +1,6 @@
 public class Sqrt {
+
+    public static float[] racine(float a, float b){
+        return new float[]{1f};
+    }
 }
--- src/test/java/SqrtTest.java
+++ src/test/java/SqrtTest.java
@@ -1,5 +1,26 @@
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SqrtTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void racineEntre11() {
+
+        float [] attendu = new float[] {1f} ;
+        assertEquals(attendu, Sqrt.racine(1,1f));
+
+    }
+
 
 }
